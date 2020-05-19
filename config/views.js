8f418d77256e21a3667c6e3b579f318506259690
Leon <leon_1122@live.cn>
2020-05-19 15:42:09 +0800

    docker配置

--- config/views.js
+++ config/views.js
@@ -17,73 +17,45 @@
 module.exports.views = {
 
   /***************************************************************************
-  *                                                                          *
-  * Extension to use for your views. When calling `res.view()` in an action, *
-  * you can leave this extension off. For example, calling                   *
-  * `res.view('homepage')` will (using default settings) look for a          *
-  * `views/homepage.ejs` file.                                               *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Extension to use for your views. When calling `res.view()` in an action, *
+   * you can leave this extension off. For example, calling                   *
+   * `res.view('homepage')` will (using default settings) look for a          *
+   * `views/homepage.ejs` file.                                               *
+   *                                                                          *
+   ***************************************************************************/
 
-   extension: 'hbs',
+  extension: 'hbs',
 
   /***************************************************************************
-  *                                                                          *
-  * The path (relative to the views directory, and without extension) to     *
-  * the default layout file to use, or `false` to disable layouts entirely.  *
-  *                                                                          *
-  * Note that layouts only work with the built-in EJS view engine!           *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * The path (relative to the views directory, and without extension) to     *
+   * the default layout file to use, or `false` to disable layouts entirely.  *
+   *                                                                          *
+   * Note that layouts only work with the built-in EJS view engine!           *
+   *                                                                          *
+   ***************************************************************************/
 
   layout: false,
 
   getRenderFn: () => {
     // Import `consolidate`.
     var cons = require('consolidate'),
-        handlebars = require('handlebars'),
-        layouts = require('handlebars-layouts'),
-        handlebarsRegistrar = require('handlebars-registrar'),
-        fs = require("fs"),
-        path = require("path");
+      handlebars = require('handlebars'),
+      handlebarsWax = require('handlebars-wax'),
+      path =require('path');
 
     // Register helpers
-    handlebars.registerHelper(layouts(handlebars));
-    handlebarsRegistrar(handlebars, {
-      helpers: path.resolve(__dirname, '../views/helpers') + '/*-helper.js',
-      bustCache: true,
-    });
+    handlebarsWax(handlebars)
+      .helpers(require('handlebars-layouts'))
+      .helpers(path.resolve(__dirname, '../views/helpers') + '/*-helper.js');
 
     // Register partials
     var layoutsDir = path.resolve(__dirname, '../views/layouts'),
-        partialsDir = path.resolve(__dirname, '../views/partials'),
-        partials = {};
-    readDirSync(path.join(layoutsDir));
-    readDirSync(path.join(partialsDir));
-
-    function readDirSync(dir){
-      var pa = fs.readdirSync(dir);
-      pa.forEach(function(ele,index){
-        var info = fs.statSync(dir+"/"+ele)
-        if(info.isDirectory()){
-          readDirSync(dir+"/"+ele);
-        }else{
-          if (path.extname(ele) === '.hbs') {
-            if (ele.indexOf('layout') >= 0) {
-              partials[path.basename(ele, '.hbs')] = fs.readFileSync(dir+"/"+ele, 'utf8');
-            } else {
-              var relpath = path.relative(partialsDir, dir);
-              var key = (relpath + '/' + path.basename(ele, '.hbs')).replace(/\\/g,'/')
-              partials[key] = fs.readFileSync(dir+"/"+ele, 'utf8');
-            }
-          }
-        }
-      })
-    }
-
-    for (var partial in partials) {
-      handlebars.registerPartial(partial, partials[partial]);
-    }
+      partialsDir = path.resolve(__dirname, '../views/partials');
+    handlebarsWax(handlebars)
+      .partials(path.join(layoutsDir) + '/**/*.hbs')
+      .partials(path.join(partialsDir) + '/**/*.hbs');
 
     cons.requires.handlebars = handlebars;
 
